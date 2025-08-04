073632e990cb8f046caada38521508c0d2bf5d48
AlexFerRod86 <alexferrod86@gmail.com>
2025-08-04 19:31:58 +0200

    varios dias haciendo ejercicios hasta hoy no los subi

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    if (num < 0) {return "ERROR"        
+    }
+    let word =""
+    for (let index = 0; index < num; index++) {
+        word += string
+    }
+    return word ;
 };
+
+const num = function() { 
+    let num = Math.random * 1000;
+    let numfloor = num * Math.floor;
+        
+    return numfloor ;
+}
+        
+        
+    
+
+ repeatString('bye', num());
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return word.split("").reverse().join("")
 };
-
+reverseString("hello")
 // Do not edit below this line
 module.exports = reverseString;
