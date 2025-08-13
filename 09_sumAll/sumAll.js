6eb4446a94539aff1cd3e77bce25ee026cfd42d6
AlexFerRod86 <alexferrod86@gmail.com>
2025-08-13 16:29:57 +0200

    "Esta vez tuve que buscar mucho en google"

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) {
+        console.log("ERROR");
+        return "ERROR"
+    }
+    if (!Number.isFinite(a) || !Number.isFinite(b)) {
+        console.log("ERROR");
+        return "ERROR";
+        
+    }
+    if (!Number.isInteger(a)|| !Number.isInteger(b)) {
+        console.log("ERROR");
+        return "ERROR";
+    }
 
-};
+    let sumArray = []
+    let start = Math.min(a, b)
+    let end = Math.max(a, b)
+    for (let index = start; index <= end; index++) {
+        sumArray.push(index)
+
+    }
+    let sum = sumArray.reduce((acum, valorAct) => acum + valorAct)
+
+
+
+
+
+    return sum;
+
+
+
+}
+    ;
+
+sumAll(1, 4);
+//console.log(sum);
 
 // Do not edit below this line
 module.exports = sumAll;
