09b7494d3c997a2b4ef3f48b7297890f039e16af
AlexFerRod86 <alexferrod86@gmail.com>
2025-08-14 13:32:29 +0200

    función simplificada a la de la solución

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(a) {
+    if ((a % 4 === 0 && a % 100 !==0) ||( a % 400 === 0)) {
+         return true;      
+    
+    }else{ 
+        return false;
+        
+    }
+}
+leapYears(1400);
 // Do not edit below this line
 module.exports = leapYears;
