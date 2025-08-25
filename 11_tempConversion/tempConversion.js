0760e9b2cdcee7af131ed28409551571c34a4b59
AlexFerRod86 <alexferrod86@gmail.com>
2025-08-25 12:39:38 +0200

    Conversion de grados a fahrenheit y viceversa

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,10 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  let Celsius = 
+  (a - 32) * (5/9)
+  if (Number.isInteger(Celsius)) {
+    return Celsius
+    
+  } else { return Number (Celsius.toFixed(1))
+    
+  }
+    
+  }
+
+
+const convertToFahrenheit = function(b) {
+  let Fahrenheit = 
+  (b * (9/5)) + 32
+  if (Number.isInteger(Fahrenheit)) {
+    return Fahrenheit
+    
+  } else { return Number(Fahrenheit.toFixed(1))
+    
+  }
 };
 
-const convertToFahrenheit = function() {
-};
+convertToCelsius(100)
+convertToFahrenheit(0)
 
-// Do not edit below this line
+// Do not 0000000000edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
